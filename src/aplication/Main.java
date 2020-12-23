a9b4522ba3548b1660b252d841cdfe307b7bef10
Alef Bispo <alef.b.dos.santos@hotmail.com>
2020-12-23 20:50:51 -0300

    passe posicao
    criando primeira classe

--- src/aplication/Main.java
+++ src/aplication/Main.java
@@ -1,8 +1,17 @@
 package aplication;
 
+import aplication.tabuleiro.Posicao;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan = new Scanner(System.in);
+
+        Posicao posicao = new Posicao(3, 5);
+        System.out.println(posicao);
+
+        scan.close();
     }
 }
